4bf7a9c48a5ce9ee1b9889d9aad39e6daba588c3
Florian <floriandelaunay75@gmail.com>
2025-05-05 11:15:56 +0200

    gestion du format date du twwet, test ok, affiche le tweet depuis son heure de création

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -1,58 +1,74 @@
-import styles from '../styles/LastTweets.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react'
+import styles from "../styles/LastTweets.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import react from "react";
 
 function LastTweets(props) {
-    const [likesCounter, setLikesCounter] = useState(0)
-    const [heartColor, setHeartColor] = useState({color: ''})
-    const heart = <FontAwesomeIcon icon={faHeart} className={styles.heart} style={heartColor} />
-    const deleteBtn = <FontAwesomeIcon icon={faTrash} className={styles.deleteBtn} />
-    
-    // Add the color and the number of like
-    function tweetLiked() {
-        if (likesCounter < 1) {
-            setLikesCounter(likesCounter + 1)
-            setHeartColor({color: 'rgba(226, 38, 77, 1)'})
-        } else if (likesCounter === 1){
-            setLikesCounter(likesCounter - 1)
-            setHeartColor({color: ''})
-        }
+  const [likesCounter, setLikesCounter] = useState(0);
+  const [heartColor, setHeartColor] = useState({ color: "" });
+  const heart = (
+    <FontAwesomeIcon
+      icon={faHeart}
+      className={styles.heart}
+      style={heartColor}
+    />
+  );
+  const deleteBtn = (
+    <FontAwesomeIcon icon={faTrash} className={styles.deleteBtn} />
+  );
+
+  // Add the color and the number of like
+  function tweetLiked() {
+    if (likesCounter < 1) {
+      setLikesCounter(likesCounter + 1);
+      setHeartColor({ color: "rgba(226, 38, 77, 1)" });
+    } else if (likesCounter === 1) {
+      setLikesCounter(likesCounter - 1);
+      setHeartColor({ color: "" });
     }
+  }
 
-    // Delete the tweet
-    function deleteTweet() {
-        const tweetToDelete = async () => {
-            const response= await fetch("http://localhost:3000/tweet/6814e27677497ff6b14a158e", {
-                method: 'DELETE',
-                headers: {'Content-Type':'application/json'},
-                body: JSON.stringify({})
-            })
-            const data= await response.json();
-            console.log(data)
+  // Delete the tweet
+  function deleteTweet() {
+    const tweetToDelete = async () => {
+      const response = await fetch(
+        "http://localhost:3000/tweet/6814e27677497ff6b14a158e",
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({}),
         }
-        tweetToDelete()
+      );
+      const data = await response.json();
+      console.log(data);
     };
+    tweetToDelete();
+  }
 
-    return(
-        <div className={styles.divContainer}>
-            <div className={styles.LastTweetsContainer}>
-                <div className={styles.title}>
-                    <div className={styles.userLogo}>
-                        <img src='userStartingIcon.webp' className={styles.logoIconUSer} />
-                    </div>
-                    <div className={styles.firstname}>Antoine</div>
-                    <div className={styles.username}>@AntoineLeProf</div>
-                    <div className={styles.timeTweet}>· 5 hours</div>
-                </div>
-                <div className={styles.message}>{props.message}</div>
-                <div className={styles.btnInfo}>
-                    <div className={styles.likes} onClick={() => tweetLiked()}>{heart} {likesCounter}</div>
-                    <div className={styles.delete} onClick={() => deleteTweet()}>{deleteBtn}</div>
-                </div>
-            </div>
+  return (
+    <div className={styles.divContainer}>
+      <div className={styles.LastTweetsContainer}>
+        <div className={styles.title}>
+          <div className={styles.userLogo}>
+            <img src="userStartingIcon.webp" className={styles.logoIconUSer} />
+          </div>
+          <div className={styles.firstname}>Antoine</div>
+          <div className={styles.username}>@AntoineLeProf</div>
+          <div className={styles.timeTweet}>· {props.date}</div>
         </div>
-    )
-};
+        <div className={styles.message}>{props.message}</div>
+        <div className={styles.btnInfo}>
+          <div className={styles.likes} onClick={() => tweetLiked()}>
+            {heart} {likesCounter}
+          </div>
+          <div className={styles.delete} onClick={() => deleteTweet()}>
+            {deleteBtn}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default LastTweets;
--- components/TweetHome.js
+++ components/TweetHome.js
@@ -1,87 +1,91 @@
-import styles from '../styles/TweetHome.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import Tweet from './Tweet';
-import Trends from './Trends';
-import LastTweets from './LastTweets';
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import styles from "../styles/TweetHome.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import Tweet from "./Tweet";
+import Trends from "./Trends";
+import LastTweets from "./LastTweets";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import moment from "moment";
 
 function TweetHome() {
-    const logoTwitter = <FontAwesomeIcon icon={faTwitter} className={styles.logoTwitter} />
-    const firstname = "Antoine";
-    const username = "@AntoineLeProf";
-    const trends = <Trends />
-    const [tweets, setTweets] = useState([])
-    const hastagList = useSelector(state => state.hastags.value)
+  const logoTwitter = (
+    <FontAwesomeIcon icon={faTwitter} className={styles.logoTwitter} />
+  );
+  const firstname = "Antoine";
+  const username = "@AntoineLeProf";
+  //   const trends = <Trends />;
+  const [tweets, setTweets] = useState([]);
+  const hastagList = useSelector((state) => state.hastags.value);
 
-    const fetchTweet = async () => {
-        const response = await fetch("http://localhost:3000/tweet")
-        const data = await response.json()
-        setTweets((data.allTweet).reverse());  
-    }
+  const fetchTweet = async () => {
+    const response = await fetch("http://localhost:3000/tweet");
+    const data = await response.json();
+    setTweets(data.allTweet.reverse());
+  };
 
-    // Taking the data at page's initialization
-    useEffect(() => {
-        fetchTweet()
-    }, [])
+  // Taking the data at page's initialization
+  useEffect(() => {
+    fetchTweet();
+  }, []);
 
-    // Refresh when tweeted with Props
-    const refreshingData = () => {
-        fetchTweet()
-    };
+  // Refresh when tweeted with Props
+  const refreshingData = () => {
+    fetchTweet();
+  };
 
-    const lastTweets = tweets.map((element, index) => {
-        return <LastTweets key={index} message={element.message} />
-    });
+  const lastTweets = tweets.map((element, index) => {
+    let format = moment(element.date).fromNow();
 
-    const tweet = <Tweet refreshingData={refreshingData} />;
+    return <LastTweets key={index} message={element.message} date={format} />;
+  });
 
-    // To do --> Checking the # 
+  const tweet = <Tweet refreshingData={refreshingData} />;
 
-    // const trends = hastagList.map((element, index) => {
-    //     return <Trends key={index} hastag={element.hastag} />
-    // })
+  const trends = hastagList.map((element, index) => {
+    return <Trends key={index} hastag={element.hastag} />;
+  });
 
-    return (
-        <div className={styles.mainDiv}>
-            <div className={styles.container}>
-                <div className={styles.leftContainer}>
-                    <div className={styles.logoLeftContainer}>
-                        {logoTwitter}
-                    </div>
-                    <div className={styles.infoLeftContainer}>
-                        <div className={styles.infoLeftContainerGroupingInfo}>
-                            <div className={styles.infoLeftContainerLogoUser}>
-                                <img src='userStartingIcon.webp' className={styles.logoIconUser} />
-                            </div>
-                            <div className={styles.infoLeftContainerId}>
-                                <div className={styles.infoLeftContainerFirstname}>{firstname}</div>
-                                <div className={styles.infoLeftContainerUsername}>{username}</div>
-                            </div>
-                        </div>
-                        <button className={styles.infoLeftContainerBtnLogout}>Logout</button>
-                    </div>
+  return (
+    <div className={styles.mainDiv}>
+      <div className={styles.container}>
+        <div className={styles.leftContainer}>
+          <div className={styles.logoLeftContainer}>{logoTwitter}</div>
+          <div className={styles.infoLeftContainer}>
+            <div className={styles.infoLeftContainerGroupingInfo}>
+              <div className={styles.infoLeftContainerLogoUser}>
+                <img
+                  src="userStartingIcon.webp"
+                  className={styles.logoIconUser}
+                />
+              </div>
+              <div className={styles.infoLeftContainerId}>
+                <div className={styles.infoLeftContainerFirstname}>
+                  {firstname}
                 </div>
-                <div className={styles.rightContainer}>
-                    {/* Componenent Tweet */}
-                    <div className={styles.twitterContent}>
-                        <div className={styles.tweetToWrite}>
-                            {tweet}
-                        </div>
-                        {/* Componenent Last Tweet */}
-                        <div className={styles.lastTweet}>
-                            {lastTweets}
-                        </div>
-                    </div>
+                <div className={styles.infoLeftContainerUsername}>
+                  {username}
                 </div>
-                {/* Component Trends */}
-                <div className={styles.trends}>
-                    {trends}
-                </div>
+              </div>
             </div>
+            <button className={styles.infoLeftContainerBtnLogout}>
+              Logout
+            </button>
+          </div>
         </div>
-    );
+        <div className={styles.rightContainer}>
+          {/* Componenent Tweet */}
+          <div className={styles.twitterContent}>
+            <div className={styles.tweetToWrite}>{tweet}</div>
+            {/* Componenent Last Tweet */}
+            <div className={styles.lastTweet}>{lastTweets}</div>
+          </div>
+        </div>
+        {/* Component Trends */}
+        <div className={styles.trends}>{trends}</div>
+      </div>
+    </div>
+  );
 }
 
 export default TweetHome;
